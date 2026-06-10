b1ceb2ce26c369fa7bcb2712da787b2895e71f2a
Jenna Grieshop <47341279+jengriesh@users.noreply.github.com>
2026-06-10 14:37:18 -0500

    Create Row_Column_Extraction_Simplified.m
    This is a simplified version of Row Column Extraction. This extracts the N, T, S, I strips through the CDC for each subject. No averaging

---
+++ MAP_Analysis/Row_Column_Extraction_Simplified.m
@@ -0,0 +1,193 @@
+% Row and Column Extraction Simplified
+%
+% Created by: Jenna Grieshop
+% Date created: 6/10/2026
+%
+% Description: Extracts individual rows and columns at the CDC
+% coordinates for each subject. 
+%
+% Inputs: User selects directory containing matrices in mat format
+% (could be results from Stdev_Maps, matrix subtraction, etc.). NOTE: if
+% the matrices are not bound density mm matrices, change the file
+% identifier on line 25.
+% 
+% Then user selects the PCD CDC analysis summary file with OS/OD eye column 
+% added after the file name column. 
+%
+% Outputs: .csv files with N, T, S, I strips extracted from the density
+% matrices through the CDC
+% 
+
+close all
+clear all
+clc
+
+identifier = 'bounddensity_mm_matrix';
+
+basePath = which('Row_Column_Extraction_Simplified.m');
+
+[basePath ] = fileparts(basePath);
+path(path,fullfile(basePath,'lib')); % Add our support library to the path.
+
+% user to select directory with data
+root_path_raw = uigetdir('.','Select directory containing raw mat analyses');
+mkdir(root_path_raw,'Results');
+
+output_root = fullfile(root_path_raw,'Results');
+
+
+% get all the file paths that we are interested in in each of the folders
+[file_names] = read_folder_contents(root_path_raw, 'mat'); % used for stdev map
+
+% select cdc analysis file
+[filename_cdc, pathname_cdc] = uigetfile('*.csv', 'Please select the pcd cdc analysis summary file', 'MultiSelect', 'off');
+
+
+% load in cdc LUT data and extract all the subject IDs
+LUT_data = readtable(fullfile(pathname_cdc, filename_cdc));
+
+
+
+%% INDIVIDUAL SUBJECTS
+
+% go through all the subjects
+for i=1:size(file_names,1)
+
+    % need to find the index in the lut to match the item in the folder
+    for index = 1:size(LUT_data, 1)
+        trigger = find(strcmp(LUT_data{index,1},(file_names{i, end})));
+        if length(trigger) == 1
+            break
+        end
+    end
+
+    % get the scale factor
+    scale = LUT_data{index,11}; % umpp
+    eye = LUT_data{index,2};
+
+    % load in the csv data and master cdc coords
+
+    % Load in current map
+    CurrentMap = load(fullfile(root_path_raw,file_names{i}));
+    dummyName = fieldnames(CurrentMap);
+    mat_data = CurrentMap.(dummyName{1});
+
+    cdc_x = LUT_data{index,9};
+    cdc_y = LUT_data{index,10};
+
+
+    % get row and column of the data
+
+    h_strip = mat_data(:,cdc_x);
+    v_strip = mat_data(cdc_y, :);
+
+    % create x data arrays, reformat y data
+    data.x_h = (0:length(h_strip)-1)';
+    data.x_v = (0:length(v_strip)-1)';
+    data.y_h = h_strip';
+    data.y_v = v_strip';
+    
+    % initialize matrix for the converted data
+    xy_h_converted = zeros(length(data.x_h), 2);
+    xy_v_converted = zeros(length(data.x_v), 2);
+
+    % convert the data to be all in the same orientation.
+    for j=1:length(data.x_h)
+        % check what eye - if left flip sign so that temporal is negative -
+        % need eye from the lut file to do this
+        if strcmp(eye,'OD')
+            xy_h_converted(j,1) = (data.x_h(j) - cdc_y + 1) * scale; % x values for h now in um
+        else
+            xy_h_converted(j,1) = -(data.x_h(j) - cdc_y + 1) * scale; % x values for h now in um  
+        end
+        xy_v_converted(j,1) = (data.x_v(j) - cdc_x + 1) * scale; %  x values for v now in um
+        % y values are not scaled
+        xy_h_converted(j,2) = data.y_h(j);
+        xy_v_converted(j,2) = data.y_v(j);
+    end
+
+    % store data for each subject
+    all_subjects_raw_h_rc{i} = xy_h_converted;
+    all_subjects_raw_v_rc{i} = xy_v_converted;
+
+
+%% graph individual plots
+    % basic plot of the individual results
+    % figure(1)
+    % plot(xy_h_converted(:,1), xy_h_converted(:,2));
+    % title("Horizontal Density Through CDC Point");
+    % xlabel("Microns");
+    % ylabel("Density");
+    % hold off
+    % 
+    % figure(2)
+    % plot(xy_v_converted(:,1), xy_v_converted(:,2));
+    % title("Vertical Density Through CDC Point");
+    % xlabel("Microns");
+    % ylabel("Density");
+    % hold off
+
+
+    %% split data at the cdc point
+
+    % find the 0 point in the eccentricity
+    v_0_index = find(all_subjects_raw_v_rc{1,1} == 0); 
+    h_0_index = find(all_subjects_raw_h_rc{1,1} == 0); 
+
+    % split the vertical data into top and bottom
+    v_ecc_t = -all_subjects_raw_v_rc{1,1}(1:v_0_index,1);
+    v_dens{1} = all_subjects_raw_v_rc{1,1}(1:v_0_index,2)'; % top
+
+    v_ecc_b = all_subjects_raw_v_rc{1,1}(v_0_index:end,1);
+    v_dens{2} = all_subjects_raw_v_rc{1,1}(v_0_index:end,2)'; % bottom
+
+    % split the horizontal data into left and right
+    h_ecc_l = -all_subjects_raw_h_rc{1,1}(1:h_0_index,1);
+    h_dens{1} = all_subjects_raw_h_rc{1,1}(1:h_0_index,2)'; % left
+    
+    h_ecc_r = all_subjects_raw_h_rc{1,1}(h_0_index:end,1);
+    h_dens{2} = all_subjects_raw_h_rc{1,1}(h_0_index:end,2)'; % right
+
+
+
+    % compile results and create headers for output sheet
+    results = {h_ecc_l, h_dens{1}', h_ecc_r, h_dens{2}', v_ecc_t, v_dens{1}', v_ecc_b, v_dens{2}'};
+    headers = {'Ecc_Horz_Temporal_um', 'Density_Horz_Temporal', 'Ecc_Horz_Nasal_um', 'Density_Horz_Nasal', 'Ecc_Vert_Superior_um,', 'Density_Vert_Superior', 'Ecc_Vert_Inferior_um', 'Density_Vert_Inferior'};
+    
+    %% Plotting for sanity check
+    % figure(3)
+    % plot(v_ecc_t, v_dens{1});
+    % hold on
+    % plot(v_ecc_b, v_dens{2});
+    % plot(h_ecc_l, h_dens{1});
+    % plot(h_ecc_r, h_dens{2});
+
+
+    %% ---- Save output ----
+
+    % create the output file name
+    outname = strcat(file_names{i},'_', eye, '_HorizVertDensity_', string(datetime('now','TimeZone','local','Format','yyyyMMdd_hhmmss')), '.csv');
+    outfile = fullfile(output_root, outname);
+
+    % organize all the data into a format that can be saved to the csv
+    D = {};
+    for k = 1:numel(results)
+      D(1:numel(results{k}),k)=num2cell(results{k});
+    end
+    combined_output = [headers; D];
+    writecell(combined_output, outfile);
+
+
+end
+
+%% save raw data for all subjects
+
+% fname_h_all = fullfile(pathname_cdc, 'all_h_rc_data.mat');
+% fname_v_all = fullfile(pathname_cdc, 'all_v_rc_data.mat');
+% save(fname_h_all, 'all_subjects_raw_h_rc');
+% save(fname_v_all, 'all_subjects_raw_v_rc');
+
+
+
+
+
